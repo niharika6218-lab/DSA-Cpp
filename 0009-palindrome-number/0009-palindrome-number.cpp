491d2f5d49d2d8d50556fb403348a2f4d85e4986
niharika6218-lab <niharika6218@gmail.com>
2026-09-16 12:20:34 +0530

    Time: 16 ms (5.29%), Space: 10.9 MB (11.93%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -6,12 +6,11 @@ public:
         int first=0;
         int last=length-1;
         while(first<last){
-            swap(n[first++],n[last--]);
-            /*char temp=n[first];
+            char temp=n[first];
             n[first]=n[last];
             n[last]=temp;
             first++;
-            last--;*/
+            last--;
         }
         if(to_string(x)==n){
             return true;
